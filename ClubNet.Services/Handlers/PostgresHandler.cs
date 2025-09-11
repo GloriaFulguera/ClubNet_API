9c05443680927b5745d082b28b21b2f16ac8db4a
Gloria Fulguera <gloriafulguera@gmail.com>
2025-09-11 09:32:17 -0300

    aplico gitignore para dejar de trackear appsettings

---
+++ ClubNet.Services/Handlers/PostgresHandler.cs
@@ -0,0 +1,29 @@
+﻿using Npgsql;
+
+namespace ClubNet.Services.Handlers
+{
+    public class PostgresHandler
+    {
+        public static string ConnectionString = "Host=localhost;Port=5432;Database=clubnet;Username=postgres;Password=12345678";
+
+        public static bool Exec(string query)
+        {
+            bool result = false;
+            try
+            {
+                using (var conn = new NpgsqlConnection(ConnectionString))
+                {
+                    var cmd = new NpgsqlCommand(query, conn);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    result = true;
+                }
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
