37988b07551677e5a9f6be7ffd55a621c1faec7b
Gloria Fulguera <gloriafulguera@gmail.com>
2025-10-01 23:19:02 -0300

    Agrego controlador para enpoint usuarios e inyeccion de dependencias

---
+++ ClubNet.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using ClubNet.Models.DTO;
+using ClubNet.Services.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClubNet.Api.Controllers
+{
+    [Route("api/usuario")]
+    [ApiController]
+    public class UsuarioController:ControllerBase
+    {
+        private readonly IUsuariosRepository _usuarioService;
+
+        public UsuarioController(IUsuariosRepository usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+        [HttpGet("GetUsuario")]
+        public async Task<UsuarioDTO> GetUsuarioById(int id)
+        {
+            return await Task.Run(() => _usuarioService.GetUsuarioById(id));
+        }
+    }
+}
