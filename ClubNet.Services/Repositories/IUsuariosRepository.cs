73d28a23b803de6893a49c395087b58bc0e3f59e
Gloria Fulguera <gloriafulguera@gmail.com>
2025-10-01 23:18:27 -0300

    Armo modelo para deseralizar la info de los usuarios y armo el llamado a base de datos

---
+++ ClubNet.Services/Repositories/IUsuariosRepository.cs
@@ -0,0 +1,9 @@
+﻿using ClubNet.Models.DTO;
+
+namespace ClubNet.Services.Repositories
+{
+    public interface IUsuariosRepository
+    {
+        public Task<UsuarioDTO> GetUsuarioById(int id);
+    }
+}
