77100b57e14e12843b05c7d3c182b0727c4ba1cf
Gloria Fulguera <gloriafulguera@gmail.com>
2025-10-23 01:06:57 -0300

    Armo el repositorio de metodos e implemento la interfaz

---
+++ ClubNet.Services/Repositories/IClasesRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using ClubNet.Models;
+using ClubNet.Models.DTO;
+
+namespace ClubNet.Services.Repositories
+{
+    public interface IClasesRepository
+    {
+        public ApiResponse CreateClase(CreateClaseDTO clase);
+        public ApiResponse UpdateClase(ClaseDTO clase);
+        public ApiResponse<List<ClaseDTO>> GetClases(int actividadId);
+        public ApiResponse DeleteClase(int claseId);
+    }
+}
