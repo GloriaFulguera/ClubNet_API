6140e0f138559c88df3169242d5955e860f97e5a
Gloria Fulguera <gloriafulguera@gmail.com>
2025-10-23 01:07:52 -0300

    Inyeccion de dependencias y controlador de endpoints

---
+++ ClubNet.Api/Controllers/ClaseController.cs
@@ -0,0 +1,58 @@
+﻿using ClubNet.Models.DTO;
+using ClubNet.Services.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClubNet.Api.Controllers
+{
+    [ApiController]
+    [Route("api/clase")]
+    public class ClaseController:ControllerBase
+    {
+        private readonly IClasesRepository _claseService;
+
+        public ClaseController(IClasesRepository claseService)
+        {
+            _claseService = claseService;
+        }
+
+        [HttpPost("CreateClase")]
+        public IActionResult CreateClase(CreateClaseDTO clase)
+        {
+            var result = _claseService.CreateClase(clase);
+            if (result.Success)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+
+        [HttpPut("UpdateClase")]
+        public IActionResult UpdateClase(ClaseDTO clase)
+        {
+            var result = _claseService.UpdateClase(clase);
+            if (result.Success)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+
+        [HttpGet("GetClases")]
+        public IActionResult GetClases(int actividadId)
+        {
+            var result = _claseService.GetClases(actividadId);
+            if (result.Success)
+                return Ok(result.Data);
+            else
+                return BadRequest(result);
+        }
+
+        [HttpDelete("DeleteClase")]
+        public IActionResult DeleteClase(int claseId)
+        {
+            var result = _claseService.DeleteClase(claseId);
+            if (result.Success)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+    }
+}
